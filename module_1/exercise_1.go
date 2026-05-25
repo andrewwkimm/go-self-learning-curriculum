9eba56049cd7a4d692a735408d8ea4301a12a204
andrewwkimm <andrewkimka@gmail.com>
2026-05-25 05:45:33 -0400

    Start exercise 1

---
+++ module_1/exercise_1.go
@@ -0,0 +1,11 @@
+package module1
+
+import "fmt"
+
+func exercise_1() {
+	var foo float32
+	var bar float64
+
+	fmt.Println(foo)
+	fmt.Println(bar)
+}
---
+++ module_1/exercise_1_test.go
@@ -0,0 +1,7 @@
+package module1
+
+import "testing"
+
+func TestExercise1(t *testing.T) {
+	exercise_1()
+}
